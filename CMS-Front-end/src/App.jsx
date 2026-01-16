27c8226bc38257cf75a188a9d4e6c30a9fd8d3c7
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-01-16 13:50:33 +0530

    Setting up new project

---
+++ CMS-Front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-red-500 text-2xl min-h-screen flex items-center justify-center">
+      Tailwind is working
+    </div>
+  )
+}
+
+export default App
