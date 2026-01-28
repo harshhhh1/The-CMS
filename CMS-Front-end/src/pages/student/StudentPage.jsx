6f52425bcb0099d7836483d6310f738f9f211c96
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-01-28 20:04:39 +0530

    created the big calendar

--- CMS-Front-end/src/pages/student/StudentPage.jsx
+++ CMS-Front-end/src/pages/student/StudentPage.jsx
@@ -1,5 +1,6 @@
 import { Announcements } from "../../components/Announcements";
 import { EventCalender } from "../../components/EventCalender";
+import { BigCalendar } from "../../components/BigCalendar";
 import { Layout } from "../Layout";
 
 export function StudentPage() {
@@ -8,7 +9,10 @@ export function StudentPage() {
             <div className=" flex flex-col xl:flex-row gap-4 p-4 h-full overflow-hidden scroll-none m-0">
                 {/* LEFT SIDE */}
                 <div className="w-full lg:w-2/3 flex flex-col gap-4 h-[95%] overflow-y-auto overscroll-contain">
-                    <div className="h-full bg-white p-4 rounded-md"></div>
+                    <div className="h-full bg-white p-4 rounded-md">
+                        <h1 className="text-lg font-semibold">Schedule (4A)</h1>
+                        <BigCalendar />
+                    </div>
                 </div>
                 {/* RIGHT SIDE */}
                 <div className="w-full lg:w-1/3 flex flex-col gap-4 h-[95%] overflow-y-auto overscroll-contain">
