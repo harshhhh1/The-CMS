147f335afcab8d643dbbec009d328c7eaf55bed0
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-02-02 19:08:21 +0530

    created table for teachers

--- CMS-Front-end/src/App.jsx
+++ CMS-Front-end/src/App.jsx
@@ -1,8 +1,11 @@
 import { AdminPage } from './pages/adminpage/AdminPage.jsx';
 import { TeacherPage } from './pages/teacher/TeacherPage.jsx';
 import { StudentPage } from './pages/student/StudentPage.jsx';
+import { TeacherListPage } from './pages/list/teacher/page.jsx';
+import { ParentListPage } from './pages/list/parent/page.jsx';
 import { Route,Routes } from 'react-router-dom';
 import './App.css'
+import { StudentListPage } from './pages/list/student/page.jsx';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Route path="/admin" element={<AdminPage />} />
       <Route path="/teacher" element={<TeacherPage />} />
       <Route path="/student" element={<StudentPage />} />
+      <Route path="/list/teachers" element={<TeacherListPage />} />
+      <Route path="/list/students" element={<StudentListPage />} />
+      <Route path="/list/parents" element={<ParentListPage />} />
     </Routes>
   )
 }
